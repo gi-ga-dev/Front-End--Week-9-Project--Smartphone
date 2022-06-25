37ddaf86b207f5feac96a7e04e96914ce2d036e2
gi-ga-dev <cloud.gianluca@gmail.com>
2022-06-25 21:43:24 +0200

    timer fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,108 @@
 "use strict";
 const CONT = document.querySelector('.container');
 class Cellular {
-    constructor(model, creditDoll, creditCent, timeHrs, timeMin, timeSec) {
-        this._timeHrs = 0; // tempi di chiamata (ore)
-        this._timeMin = 0; // tempi di chiamata (minuti)
-        this._timeSec = 0; // tempi di chiamata (secondi)
-        this.timer = false; // var timer chiamata
+    constructor(model) {
+        this._credit = 0; // credito totale disponibile
+        this._timeHrs = 0; // tempi di chiamata corrente ore (intervallo)
+        this._timeMin = 0; // tempi di chiamata corrente minuti (intervallo)
+        this._timeSec = 0; // tempi di chiamata corrente secondi (intervallo)
+        this._callInit = false; // inizializzatore timer chiamata
+        this._timer = 0; // 
         this._model = model;
-        this._creditDoll = creditDoll;
-        this._creditCent = creditCent;
-        this._timeHrs = timeHrs;
-        this._timeMin = timeMin;
-        this._timeSec = timeSec;
     }
+    // utilizzare i get solo per definire propr. fisse che avranno tutti gli oggetti creati
     get model() { return this._model; }
-    get creditDoll() { return this._creditDoll; }
-    get creditCent() { return this._creditCent; }
-    get timeHrs() { return this._timeHrs; }
-    get timeMin() { return this._timeMin; }
-    get timeSec() { return this._timeSec; }
-    creditValue(value) { this._creditDoll = this._creditDoll + value; }
+    infoModel() {
+        return `Modello Cellulare: ${this.model}`;
+    }
+    infoCredit() {
+        return `Credito Residuo: $ ${this._credit.toFixed(2)}`;
+    }
+    infoCall() {
+        return `Tempo di chiamata: ${this._timeHrs}h ${this._timeMin}min ${this._timeSec}sec`;
+    }
+    // Metodo per caricare saldo (dollari) disponibile:
+    creditValue(value) { this._credit = this._credit + value; }
+    // Metodo per gestire logica intervallo chiamata:
     setCall() {
-        if (this.timer === true) {
+        // il timer incrementa, i soldi decrementano
+        if (this._callInit === true) {
             this._timeSec++;
-            if (this._timeSec >= 60) {
-                // qui scala 0.20cent ogni 60sec
-                this._timeSec = 0;
-                this._timeMin++;
-                if (this._timeMin >= 60) {
-                    this._timeMin = 0;
-                    this._timeHrs++;
+            if (this._timeSec >= 1) {
+                // se callInit true sec salgono
+                // se i sec sono sup./= 1 credito -0.20
+                // se credito 0 stop chiamata, stampa durata e alert
+                this._credit -= 0.20;
+                if (this._credit === 0) {
+                    // fermo chiamata, stampo la durata ed alert
+                    this.stopCall();
+                    console.log(this.infoCall());
+                    alert('Credito Esaurito');
                 }
             }
-            console.log(this);
+            console.log(this.infoCredit());
         }
     }
+    // Metodo per inizializzare intervallo chiamata:
     startCall() {
-        this.timer = true;
+        this.stopCall();
+        this._callInit = true;
+        this._credit -= 0.20; // scatto alla risposta
         // se si utilizza fat arrow function il this mantiene lo scope della classe padre
         // senza si riferisce all'istanza che ha chiamato l'oggetto (startCall)
-        setInterval(() => this.setCall(), 1000);
+        this._timer = setInterval(() => this.setCall(), 1000);
+    }
+    stopCall() {
+        this._callInit = false;
+        clearInterval(this._timer);
     }
 }
 Cellular.calls = 0; // contatore n. chiamate effettuate
 class Phone extends Cellular {
-    constructor(model, creditDoll, creditCent, timeHrs, timeMin, timeSec) {
-        super(model, creditDoll, creditCent, timeHrs, timeMin, timeSec);
-    }
-    infoModel() {
-        return `Modello Cellulare: ${this.model}`;
-    }
-    infoCredit() {
-        return `Credito Residuo: $ ${this.creditDoll}.${this.creditCent} - Tempi di chiamata: ${this.timeHrs}h ${this.timeMin}min ${this.timeSec}sec`;
+    constructor(model) {
+        super(model);
     }
 }
 class Smartphone extends Cellular {
-    constructor(model, creditDoll, creditCent, timeHrs, timeMin, timeSec, dataGb, dataMb, spaceGb, spaceMb) {
-        super(model, creditDoll, creditCent, timeHrs, timeMin, timeSec);
+    constructor(model, dataGb, dataMb) {
+        super(model);
+        /* ---- Propr. definite nel constructor (Dati Fissi) */
+        this._dataGb = 0; // Data disponibile (GB)
+        this._dataMb = 0; // Data disponibile (MB)
+        /* ---- Propr. non definite nel constructor (Dati Mutevoli) */
+        this._spaceGb = 0; // Spazio memoria in utilizzo GB (intervallo)
+        this._spaceMb = 0; // Spazio memoria in utilizzo MB (intervallo)
+        this._internetInit = false; // inizializzatore timer internet
         this._dataGb = dataGb;
         this._dataMb = dataMb;
-        this._spaceGb = spaceGb;
-        this._spaceMb = spaceMb;
     }
     get dataGb() { return this._dataGb; }
     get dataMb() { return this._dataMb; }
-    get spaceGb() { return this._spaceGb; }
-    get spaceMb() { return this._spaceMb; }
-    infoModel() {
-        return `Modello Cellulare: ${this.model}`;
-    }
-    infoCredit() {
-        return `Credito Residuo: $ ${this.creditDoll}.${this.creditCent} - Tempi di chiamata: ${this.timeHrs}h ${this.timeMin}min ${this.timeSec}sec`;
-    }
     infoData() {
-        return `Data Residuo: ${this.dataGb}GB ${this.dataMb}MB - Tempo di navigazione: ${this.spaceGb}GB ${this.spaceMb}MB`;
+        return `Data Residuo: ${this.dataGb}GB ${this.dataMb}MB - Tempo di navigazione: ${this._spaceGb}GB ${this._spaceMb}MB`;
+    }
+    // Metodo per caricare data (GB) disponibili:
+    chargeData(value) { this._dataGb = this._dataGb + value; }
+    // Metodo per gestire logica intervallo internet:
+    setInternet() {
+        if (this._internetInit === true) {
+            this._spaceMb++;
+            if (this._spaceMb >= 1000) {
+                this._spaceMb = 0;
+                this._spaceGb++;
+            }
+            console.log(this);
+        }
+    }
+    // Metodo per inizializzare intervallo internet:
+    startInternet() {
+        this._internetInit = true;
+        setInterval(() => this.setInternet(), 1000);
     }
 }
-let Nokia = new Phone('Nokia 3330', 20, 0, 0, 0, 0);
-let Iphone = new Smartphone('Iphone 4s', 100, 0, 0, 0, 0, 10, 0, 0, 0);
-let Samsung = new Smartphone('Samsung A50', 25, 0, 0, 0, 0, 10, 0, 0, 0);
+let Nokia = new Phone('Nokia 3330');
+let Iphone = new Smartphone('Iphone 4s', 100, 0);
+let Samsung = new Smartphone('Samsung A50', 25, 0);
 function commitCharge() {
     if (CONT !== null) {
         /* Nokia.creditValue(100); */
--- assets/ts/script.ts
+++ assets/ts/script.ts
@@ -2,57 +2,73 @@ const CONT = document.querySelector('.container');
 
 abstract class Cellular {
 
-    /* ... Feature Chiamate ... */
-    private _model: string;       // modello cellulare
-    private _creditDoll: number;  // credito disponibile (dollari)
-    private _creditCent: number;  // credito disponibile (centesimi)
-    private _timeHrs: number = 0; // tempi di chiamata (ore)
-    private _timeMin: number = 0; // tempi di chiamata (minuti)
-    private _timeSec: number = 0; // tempi di chiamata (secondi)
-    private timer: boolean = false;         // var timer chiamata
-    static calls: number = 0;     // contatore n. chiamate effettuate
+    /* ---- Propr. definite nel constructor (Dati Fissi) */
+    protected _model: string;              // modello cellulare
+    protected _credit: number = 0;         // credito totale disponibile
+    protected _timeHrs: number = 0;        // tempi di chiamata corrente ore (intervallo)
+    protected _timeMin: number = 0;        // tempi di chiamata corrente minuti (intervallo)
+    protected _timeSec: number = 0;        // tempi di chiamata corrente secondi (intervallo)
+    protected _callInit: boolean = false;  // inizializzatore timer chiamata
+    protected _timer: number = 0;          // 
+    static calls: number = 0;              // contatore n. chiamate effettuate
     
-    constructor(model: string, creditDoll: number, creditCent: number, timeHrs: number, timeMin: number, timeSec: number) {
+    constructor(model: string) {
         this._model = model;
-        this._creditDoll = creditDoll;
-        this._creditCent = creditCent;
-        this._timeHrs = timeHrs;
-        this._timeMin = timeMin;
-        this._timeSec = timeSec;
     }
+
+    // utilizzare i get solo per definire propr. fisse che avranno tutti gli oggetti creati
     get model() { return this._model; }
-    get creditDoll() { return this._creditDoll; }
-    get creditCent() { return this._creditCent; }
-    get timeHrs() { return this._timeHrs; }
-    get timeMin() { return this._timeMin; }
-    get timeSec() { return this._timeSec; }
- 
-    abstract infoModel(): string;
-    abstract infoCredit(): string;
+
+    infoModel(): string {
+        return `Modello Cellulare: ${this.model}`;
+    }
+    infoCredit(): string {
+        return `Credito Residuo: $ ${this._credit.toFixed(2)}`;
+    }
+    infoCall(): string {
+        return `Tempo di chiamata: ${this._timeHrs}h ${this._timeMin}min ${this._timeSec}sec`;
+    }
        
-    creditValue(value: number): void { this._creditDoll = this._creditDoll + value; }
+    // Metodo per caricare saldo (dollari) disponibile:
+    creditValue(value: number): void { this._credit = this._credit + value; }
     
+    // Metodo per gestire logica intervallo chiamata:
     setCall(): void {
-        if (this.timer === true) {
+        // il timer incrementa, i soldi decrementano
+        if (this._callInit === true) {
             this._timeSec++;
-            if(this._timeSec >= 60) {
-                // qui scala 0.20cent ogni 60sec
-                this._timeSec = 0;
-                this._timeMin++;
-                if(this._timeMin >= 60) {
-                    this._timeMin = 0;
-                    this._timeHrs++;
+            if(this._timeSec >= 1) {
+                // se callInit true sec salgono
+                // se i sec sono sup./= 1 credito -0.20
+                // se credito 0 stop chiamata, stampa durata e alert
+
+                this._credit -= 0.20;
+                if(this._credit === 0) {
+
+                    // fermo chiamata, stampo la durata ed alert
+                    this.stopCall();
+                    console.log(this.infoCall());
+                    alert('Credito Esaurito');
                 }
             }
-            console.log(this);
+            console.log(this.infoCredit());
         }
     }
-    
+
+    // Metodo per inizializzare intervallo chiamata:
     startCall(): void { 
-        this.timer = true;
+        this.stopCall();
+        this._callInit = true;
+        this._credit -= 0.20;  // scatto alla risposta
+
         // se si utilizza fat arrow function il this mantiene lo scope della classe padre
         // senza si riferisce all'istanza che ha chiamato l'oggetto (startCall)
-        setInterval( () => this.setCall(), 1000) 
+        this._timer = setInterval( () => this.setCall(), 1000 ) 
+    }
+
+    stopCall(): void {
+        this._callInit = false;
+        clearInterval(this._timer)
     }
 
 
@@ -62,56 +78,61 @@ abstract class Cellular {
 
 class Phone extends Cellular {
 
-    constructor(model: string, creditDoll: number, creditCent: number, timeHrs: number, timeMin: number, timeSec: number) {
-        super(model, creditDoll, creditCent, timeHrs, timeMin, timeSec);
-    }
-    
-    infoModel(): string {
-        return `Modello Cellulare: ${this.model}`;
-    }
-    infoCredit(): string {
-        return `Credito Residuo: $ ${this.creditDoll}.${this.creditCent} - Tempi di chiamata: ${this.timeHrs}h ${this.timeMin}min ${this.timeSec}sec`;
+    constructor(model: string) {
+        super(model);
     }
 }
 
 class Smartphone extends Cellular {
 
-    /* ... Feature Internet ... */
-    private _dataGb: number;  // Data disponibile (GB)
-    private _dataMb: number;  // Data disponibile (MB)
-    private _spaceGb: number; // Spazio memoria utilizzato (GB)
-    private _spaceMb: number; // Spazio memoria utilizzato (MB)
+    /* ---- Propr. definite nel constructor (Dati Fissi) */
+    protected _dataGb: number = 0;  // Data disponibile (GB)
+    protected _dataMb: number = 0;  // Data disponibile (MB)
 
-    constructor(model: string, creditDoll: number, creditCent: number, timeHrs: number, timeMin: number, timeSec: number, dataGb: number, dataMb: number, spaceGb: number, spaceMb: number) {
-        super(model, creditDoll, creditCent, timeHrs, timeMin, timeSec);
+    /* ---- Propr. non definite nel constructor (Dati Mutevoli) */
+    protected _spaceGb: number = 0;           // Spazio memoria in utilizzo GB (intervallo)
+    protected _spaceMb: number = 0;           // Spazio memoria in utilizzo MB (intervallo)
+    protected _internetInit: boolean = false; // inizializzatore timer internet
+
+    constructor(model: string, dataGb: number, dataMb: number) {
+        super(model);
         this._dataGb = dataGb;
         this._dataMb = dataMb;
-        this._spaceGb = spaceGb;
-        this._spaceMb = spaceMb;
     }
 
     get dataGb() { return this._dataGb; }
     get dataMb() { return this._dataMb; }
-    get spaceGb() { return this._spaceGb; }
-    get spaceMb() { return this._spaceMb; }
     
-    infoModel(): string {
-        return `Modello Cellulare: ${this.model}`;
-    }
-    infoCredit(): string {
-        return `Credito Residuo: $ ${this.creditDoll}.${this.creditCent} - Tempi di chiamata: ${this.timeHrs}h ${this.timeMin}min ${this.timeSec}sec`;
-    }
     infoData(): string {
-        return `Data Residuo: ${this.dataGb}GB ${this.dataMb}MB - Tempo di navigazione: ${this.spaceGb}GB ${this.spaceMb}MB`;
+        return `Data Residuo: ${this.dataGb}GB ${this.dataMb}MB - Tempo di navigazione: ${this._spaceGb}GB ${this._spaceMb}MB`;
     }
 
-/*     chargeData(): number {}
-    startInternet(): number {} */
+    // Metodo per caricare data (GB) disponibili:
+    chargeData(value: number): void { this._dataGb = this._dataGb + value; }
+
+    // Metodo per gestire logica intervallo internet:
+    setInternet(): void {
+        if (this._internetInit === true) {
+
+            this._spaceMb++;
+            if(this._spaceMb >= 1000) {
+                this._spaceMb = 0;
+                this._spaceGb++;
+            }
+            console.log(this);
+        }
+    }
+
+    // Metodo per inizializzare intervallo internet:
+    startInternet(): void {
+        this._internetInit = true;
+        setInterval( () => this.setInternet(), 1000 )
+    }
 }
 
-let Nokia = new Phone ('Nokia 3330', 20, 0, 0, 0, 0);
-let Iphone = new Smartphone ('Iphone 4s', 100, 0, 0, 0, 0, 10, 0, 0, 0); 
-let Samsung = new Smartphone ('Samsung A50', 25, 0, 0, 0, 0, 10, 0, 0, 0);
+let Nokia = new Phone ('Nokia 3330');
+let Iphone = new Smartphone ('Iphone 4s', 100, 0); 
+let Samsung = new Smartphone ('Samsung A50', 25, 0);
 
 function commitCharge() {
     if (CONT !== null) {
